cb45c60b10adab88b7606f36492b01cda71c6eb7
muratb1lg1n <68399694+muratb1lg1n@users.noreply.github.com>
2020-09-13 22:34:27 +0300

    timeleft

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 var tarih = new Date();
-
+var ay, gun, saat, dakika, saniye;
 
 
 var app = new Vue({
     el: '#app',
     data: {
       message: 'Hello Vue!',
-      hours: "",
-      mins: "",
-      secs:"",
-      gun : tarih.getSeconds(),
-
     },
     methods: {
-      yeni(gun){
-        setInterval(gun, 1000);
-        console.log(gun);
-      }
+      
       },
-  })
+      
+      computed : {
+        
+      }
+  });
+function yeni(){
+  const currentDate = new Date();
+  const myBirthday = new Date('7 Nov 2020');
+  var saniye = Math.floor(((myBirthday - currentDate) / 1000) % 60);
+  var dakika = Math.floor(((myBirthday - currentDate) / 1000 / 60) % 60);
+  var saat = Math.floor(((myBirthday - currentDate) / 1000 / 60 / 60) % 24);
+  var gun  = Math.floor(((myBirthday - currentDate) / 1000 / 60 / 60 / 24) % 30);
+  var ay = Math.floor((myBirthday - currentDate) / 1000 / 60 / 60 / 24 / 30);
+  return [ay, gun, saat, dakika, saniye];
+};
+
+
+
+var values = yeni();
+values.forEach(eleman => { console.log(eleman)
+
+});
+
+setInterval(yeni, 1000)
