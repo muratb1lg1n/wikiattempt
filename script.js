0f058c7ea34bee6fa686116afd0c4771b645cae0
muratb1lg1n <68399694+muratb1lg1n@users.noreply.github.com>
2020-09-15 13:04:25 +0300

    doneiguess

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-var tarih = new Date();
-var ay, gun, saat, dakika, saniye;
+var gunEl = document.getElementById('gun');
+var saatEl = document.getElementById('saat');
+var dakikaEl = document.getElementById('dakika');  
+var saniyeEl = document.getElementById('saniye');
 
 
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!',
-    },
-    methods:{
-      yeni(){
-        const currentDate = new Date();
-        const myBirthday = new Date('7 Nov 2020');
-        var saniye = Math.floor(((myBirthday - currentDate) / 1000) % 60);
-        var dakika = Math.floor(((myBirthday - currentDate) / 1000 / 60) % 60);
-        var saat = Math.floor(((myBirthday - currentDate) / 1000 / 60 / 60) % 24);
-        var gun  = Math.floor(((myBirthday - currentDate) / 1000 / 60 / 60 / 24) % 30);
-        var ay = Math.floor((myBirthday - currentDate) / 1000 / 60 / 60 / 24 / 30);
-        console.log([ay, gun, saat, dakika, saniye]);
-        setInterval(()=>{console.log('selam')}, 1000);
-      }
-    },
-  });
+function yeni(){
+  const currentDate = new Date();
+  const myBirthday = new Date('Nov 7 2020 17:45:00 GMT+0300 (GMT+03:00)');
+  
+  var totalsaniye = Math.floor((myBirthday - currentDate) / 1000);
+  
+  var saniye = Math.floor(totalsaniye % 60);
+  var dakika = Math.floor((totalsaniye / 60) % 60);
+  var saat = Math.floor((totalsaniye / 60 / 60) % 24);
+  var gun  = Math.floor(totalsaniye / 60 / 60 / 24);
+
+  gunEl.innerHTML = gun;
+  saatEl.innerHTML = saat;
+  dakikaEl.innerHTML = dakika;
+  saniyeEl.innerHTML = saniye;
+
+
+
+};
+
+yeni();
+
+setInterval(yeni , 1000);
